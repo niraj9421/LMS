a34c625ef8343ca125ad24902bcce0454b1e281e
niraj9421 <patilniraj147@gmail.com>
2025-07-30 20:05:01 +0530

    Fixed error

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,20 +1,21 @@
-import express from "mongoose";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        _id:{type: String, required: true},
-        name: {type: String, required: true},
-        email: {type: String, required: true},
-        imageUrl: {type: String, required: true},
-        enrolledCourses: [
-            {
-                type: mongosse.Schema.Types.ObjectId,
-                ref:'Course'
-            }
-        ],
-    }, {timestamps: true}
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course',
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 
-export default User
+export default User;
